a692e2438441f3068884f831bbd368157c7f8b67
P RABIN KRISHNA <pathipatirabin@gmail.com>
2026-09-10 17:17:11 +0530

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  serverExternalPackages: ['better-sqlite3'],
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: 'images.unsplash.com' },
+    ],
+  },
+};
+
+export default nextConfig;
